8255f1d91abe1b8b9c571b0e5724e1d050b3fbdf
SidSTi <siddhant.sethi91@gmail.com>
2015-09-17 21:53:07 -0500

    tests for Binary Tree

--- tests/testDataStructures.js
+++ tests/testDataStructures.js
@@ -1,11 +1,13 @@
 var Stack = require('./../Stack.js'),
 	Queue = require('./../Queue.js'),
-	LinkedList = require('./../LinkedList.js');
+	LinkedList = require('./../LinkedList.js'),
+	BinaryTree = require('./../BinaryTree.js');
 
 
 var myStack = new Stack();
 var myQueue = new Queue();
 var myList = new LinkedList();
+var myTree = new BinaryTree();
 
 for (var i = 1; i < 6; i++) {
 	myStack.push(i);
@@ -13,9 +15,19 @@ for (var i = 1; i < 6; i++) {
 	myList.append(i);	
 }
 
-myList.addAfter(7, 2);
+var arr = [15, 12, 25, 44, 3, 1, 5];
+
+arr.forEach(function(elem) {
+	myTree.addNode(elem);
+});
+
+//console.log(myTree.root.data, myTree.root.leftChild.data, myTree.root.rightChild.data);
+
+myTree.displayInOrder(myTree.root);
+
+/*myList.addAfter(7, 2);
 
 console.log(myList.displayAll() + " and Total elements : " + myList.totalCount);
 myList.delete(2);
 console.log(myList.displayAll() + " 2nd last element in the list is : " + myList.nthLastElem(2));
-console.log(myList.indexOf(1))
+console.log(myList.indexOf(1))*/
